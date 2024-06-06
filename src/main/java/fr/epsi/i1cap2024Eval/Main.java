8bda0c4eef8ce68b83f6098023bbd10f4aa7ad0e
Aymerick772 <Aymerick772@gmail.com>
2024-06-06 19:53:35 +0200

    init project

---
+++ src/main/java/fr/epsi/i1cap2024Eval/Main.java
@@ -0,0 +1,8 @@
+package fr.epsi.i1cap2024Eval;
+
+public class Main {
+//    Note à moi => Ne pas oublier de commenter // Ajouter un ReadMe
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
